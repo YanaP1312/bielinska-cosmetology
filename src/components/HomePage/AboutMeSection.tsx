dd651f7d11accb1c0cbb86201d0fc54cf41046a3
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-08-02 23:49:30 +0300

    fix h1&Image

--- src/components/HomePage/AboutMeSection.tsx
+++ src/components/HomePage/AboutMeSection.tsx
@@ -5,13 +5,15 @@ const AboutMeSection = async () => {
 
     const t = await getTranslations("common")
 
-    return (<section className="layout-container my-8">
+    return (<section className="layout-container my-6">
        
-        <h1 className="pb-4">
+        <div className="pb-4">
             <Image src="/images/about_me/TB2.png" alt="Tetiana Bielinska logo for title" width={300} height={80}/>
-            <span className="flex justify-end pt-1 text-2xl  tracking-wider font-extrabold italic ">&nbsp;&mdash;&nbsp;{t("home_page.profession")}</span></h1>
+            <h1 aria-label="Tetiana Bielinska - cosmetologist-esthetician" className="text-2xl tracking-wider font-extrabold italic flex justify-end pt-1">
+      &mdash;&nbsp;{t("home_page.profession")}
+    </h1></div>
          
-            <Image src="/images/about_me/Tetiana_Bielinska.jpg" alt="Tetiana Bielinska foto" width={328} height={100} className="border rounded-2xl"/>
+            <Image src="/images/about_me/Tetiana_Bielinska.jpg" alt="Tetiana Bielinska foto" width={328} height={400} className="border border-primary rounded-2xl"/>
            
     </section>)
 }
